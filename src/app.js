7443afbd2039adac1c8a2d7b8c209b1a403ad321
Nosoir <noeliairace13@gmail.com>
2022-09-29 20:59:18 +0000

    Random Card Generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const palos = ["♦", "♥", "♠", "♣"];
+  const numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+
+  let randomPalos = Math.floor(Math.random() * palos.length);
+  let randomNumeros = Math.floor(Math.random() * numeros.length);
+
+  let returnPalo1 = (document.getElementById("palo-top").innerHTML =
+    palos[randomPalos]);
+  let returnNumeros = (document.getElementById("number").innerHTML =
+    numeros[randomNumeros]);
+  let returnPalo2 = (document.getElementById("palo-bottom").innerHTML =
+    palos[randomPalos]);
+
+  return {
+    returnPalo1,
+    returnNumeros,
+    returnPalo2
+  };
 };
+setTimeout("location.reload()", 10000);
